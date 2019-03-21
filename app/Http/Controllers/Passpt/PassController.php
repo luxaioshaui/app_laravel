86a84af8514b078941532ea5dc892621412b7220
supreme <supreme@localhost.localdomain>
2019-03-21 05:37:17 +0000

    aaa

---
+++ app/Http/Controllers/Passpt/PassController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Passpt;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+use App\Model\UserModel;
+use Illuminate\Support\Facades\Session;
+
+class PassController extends Controller
+{
+    //
+
+    public function passPort(){
+        echo 'okoko';
+    }
+
+}
